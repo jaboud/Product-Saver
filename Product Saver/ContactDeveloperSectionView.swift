702694ff4a5bf0b93aac628d8649348b658204b7
Justin Aboud <justin.aboud@icloud.com>
2024-03-30 10:40:14 +1100

    Added partial views for settings
    Added partial views for settings view, "AboutSectionView and "ContactDeveloperSectionView"

---
+++ Product Saver/ContactDeveloperSectionView.swift
@@ -0,0 +1,88 @@
+//
+//  ContactDeveloperSectionView.swift
+//  Product Saver
+//
+//  Created by Justin Aboud on 29/3/2024.
+//
+
+import SwiftUI
+
+struct ContactDeveloperSectionView: View {
+
+    @ObservedObject var settingsViewModel: SettingsViewModel
+    @State private var showingAlert = false
+    @State private var isActionSheetPresented = false
+
+    var body: some View {
+        Section(footer: Text("Your device details will be attached to the email for troubleshooting purposes if you choose to ask for application support. No personal data is collected or stored.")) {
+            Button(action: {
+                self.isActionSheetPresented = true
+            }) {
+                Text("Contact Developer")
+                    .foregroundColor(settingsViewModel.tintColors)
+            }
+        }
+        .actionSheet(isPresented: $isActionSheetPresented) {
+            ActionSheet(title: Text("Contact Developer"), buttons: [
+                .default(Text("Support")) {
+                    self.sendEmail(includeDeviceDetails: true, subject: "Product Saver - Support")
+                },
+                .default(Text("Feedback")) {
+                    self.sendEmail(includeDeviceDetails: false, subject: "Product Saver - Feedback")
+                },
+                .default(Text("Other")) {
+                    self.sendEmail(includeDeviceDetails: false, subject: "")
+                },
+                .cancel()
+            ])
+        }
+        .alert("Cannot find 'Mail' app", isPresented: $showingAlert){
+            Button("OK") {
+                showingAlert = false
+            }
+        }
+    message:{
+        Text("Alternatively, you can contact the developer at justin.aboud@icloud.com")
+     }
+    }
+
+    func sendEmail(includeDeviceDetails: Bool, subject: String) {
+        let email = "justin.aboud@icloud.com"
+        var body = ""
+        if includeDeviceDetails {
+            let deviceDetails = getDeviceDetails()
+            body = "\n\nDevice Details:\n\(deviceDetails)"
+        }
+        let encodedBody = body.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? ""
+        let encodedSubject = subject.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? ""
+        let mailURLString = "mailto:\(email)?subject=\(encodedSubject)&body=\(encodedBody)"
+        guard let mailURL = URL(string: mailURLString) else {
+            self.showingAlert = true
+            return
+        }
+        if !UIApplication.shared.canOpenURL(mailURL) {
+            self.showingAlert = true
+        }
+        UIApplication.shared.open(mailURL)
+    }
+
+    func getDeviceDetails() -> String {
+        var details = ""
+
+        let device = UIDevice.current
+        let modelName = device.model
+        let systemName = device.systemName
+        let systemVersion = device.systemVersion
+        let appVersion = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String ?? "Unknown"
+
+        details += "Device Model: \(modelName)\n"
+        details += "Operating System: \(systemName) \(systemVersion)\n"
+        details += "Product Saver Version: \(appVersion)"
+        return details
+    }
+
+}
+
+#Preview {
+    ContactDeveloperSectionView(settingsViewModel: SettingsViewModel())
+}
--- Product Saver/SettingsView.swift
+++ Product Saver/SettingsView.swift
@@ -11,8 +11,6 @@ import SwiftData
 struct SettingsView: View {
 
     @ObservedObject var settingsViewModel: SettingsViewModel
-    @State private var showingAlert = false
-    @State private var isActionSheetPresented = false
 
     var body: some View {
         NavigationStack {
@@ -33,100 +31,13 @@ struct SettingsView: View {
                     }
                 }
 
-                Section(header: Label("About", systemImage: "info.circle")) {
-                    VStack(alignment: .leading, spacing: 10) {
-                        Text("Product Saver")
-                            .frame(maxWidth: .infinity, alignment: .center)
-                            .bold()
-                        Image("Settings")
-                            .frame(maxWidth: .infinity, alignment: .center)
-                        VStack {
-                            Text("Version: \(Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String ?? "Unknown")")
-                                .font(.footnote)
-                                .foregroundStyle(.gray)
-                            Text("Est. 2024")
-                                .font(.footnote)
-                                .foregroundStyle(.gray)
-                        }
-                        .frame(maxWidth: .infinity, alignment: .center)
-                        Text("Designed and Developed by")
-                            .frame(maxWidth: .infinity, alignment: .center)
-                        Text("Justin Aboud")
-                            .frame(maxWidth: .infinity, alignment: .center)
-                            .bold()
-                    }
+                AboutSectionView()
 
-                }
-                Section(footer: Text("Your device details will be attached to the email for troubleshooting purposes if you choose to ask for application support. No personal data is collected or stored.")) {
-                    Button(action: {
-                        self.isActionSheetPresented = true
-                    }) {
-                        Text("Contact Developer")
-                            .foregroundColor(settingsViewModel.tintColors)
-                    }
-                }
-                .actionSheet(isPresented: $isActionSheetPresented) {
-                    ActionSheet(title: Text("Contact Developer"), buttons: [
-                        .default(Text("Support")) {
-                            self.sendEmail(includeDeviceDetails: true, subject: "Product Saver - Support")
-                        },
-                        .default(Text("Feedback")) {
-                            self.sendEmail(includeDeviceDetails: false, subject: "Product Saver - Feedback")
-                        },
-                        .default(Text("Other")) {
-                            self.sendEmail(includeDeviceDetails: false, subject: "")
-                        },
-                        .cancel()
-                    ])
-                }
-
+                ContactDeveloperSectionView(settingsViewModel: settingsViewModel)
             }
             .navigationTitle("Settings")
             .accentColor(settingsViewModel.tintColors)
-            .alert("Cannot find 'Mail' app", isPresented: $showingAlert){
-                Button("OK") {
-                    showingAlert = false
-                }
-            }
-        message:{
-            Text("Alternatively, you can contact the developer at justin.aboud@icloud.com")
-         }
         }
-    }
-
-    func sendEmail(includeDeviceDetails: Bool, subject: String) {
-        let email = "justin.aboud@icloud.com"
-        var body = ""
-        if includeDeviceDetails {
-            let deviceDetails = getDeviceDetails()
-            body = "\n\nDevice Details:\n\(deviceDetails)"
-        }
-        let encodedBody = body.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? ""
-        let encodedSubject = subject.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? ""
-        let mailURLString = "mailto:\(email)?subject=\(encodedSubject)&body=\(encodedBody)"
-        guard let mailURL = URL(string: mailURLString) else {
-            self.showingAlert = true
-            return
-        }
-        if !UIApplication.shared.canOpenURL(mailURL) {
-            self.showingAlert = true
-        }
-        UIApplication.shared.open(mailURL)
-    }
-
-    func getDeviceDetails() -> String {
-        var details = ""
-
-        let device = UIDevice.current
-        let modelName = device.model
-        let systemName = device.systemName
-        let systemVersion = device.systemVersion
-        let appVersion = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String ?? "Unknown"
-
-        details += "Device Model: \(modelName)\n"
-        details += "Operating System: \(systemName) \(systemVersion)\n"
-        details += "Product Saver Version: \(appVersion)"
-        return details
     }
 }
 
