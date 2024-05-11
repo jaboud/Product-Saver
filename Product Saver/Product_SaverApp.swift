bf622a1af7a73437257472bc1e9b9c9176908fe0
Justin Aboud <justin.aboud@icloud.com>
2024-05-11 12:50:33 +1000

    Moved Tabview to ContentView from App Struct
    Moved Tabview to ContentView from App Struct due to selectedcategories being called twice, not updating in contentview

--- Product Saver/Product_SaverApp.swift
+++ Product Saver/Product_SaverApp.swift
@@ -12,42 +12,21 @@ import SwiftData
 struct Product_SaverApp: App {
 
     @StateObject private var settings = Settings()
-    @State private var selectedCategories: Set<String> = Set()
-    @State private var selectedTab = 0
 
     var body: some Scene {
         WindowGroup {
-            TabView(selection: $selectedTab) {
-                ContentView()
-                    .tabItem {
-                        Label("Home", systemImage: selectedTab == 0 ? "house.fill" : "house")
-                            .environment(\.symbolVariants, .none)
-                    }
-                    .tag(0)
-                CategoriesView(selectedCategories: $selectedCategories)
-                    .tabItem {
-                        Label("Category", systemImage: selectedTab == 1 ? "folder.fill" : "folder")
-                            .environment(\.symbolVariants, .none)
-                    }
-                    .tag(1)
-                SettingsView(settings: settings)
-                    .tabItem {
-                        Label("Settings", systemImage: selectedTab == 2 ? "gearshape.fill" : "gearshape")
-                            .environment(\.symbolVariants, .none)
-                    }
-                    .tag(2)
-            }
-            .modelContainer(for: Product.self)
-            .environmentObject(settings)
-            .font(.system(size: settings.fontSize.value))
-            .tint(settings.tintColors)
-            .onAppear(){
-                UILabel.appearance(whenContainedInInstancesOf: [UINavigationBar.self]).adjustsFontSizeToFitWidth = true
-                settings.applyColorScheme()
-            }
-            .onReceive(NotificationCenter.default.publisher(for: UIApplication.willEnterForegroundNotification)) { _ in
-                settings.applyColorScheme()
-            }
+            ContentView()
+                .modelContainer(for: Product.self)
+                .environmentObject(settings)
+                .font(.system(size: settings.fontSize.value))
+                .tint(settings.tintColors)
+                .onAppear(){
+                    UILabel.appearance(whenContainedInInstancesOf: [UINavigationBar.self]).adjustsFontSizeToFitWidth = true
+                    settings.applyColorScheme()
+                }
+                .onReceive(NotificationCenter.default.publisher(for: UIApplication.willEnterForegroundNotification)) { _ in
+                    settings.applyColorScheme()
+                }
         }
     }
 }
