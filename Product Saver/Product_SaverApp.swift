b0f25cbda87215d8332bd88906ae2ddec8444acf
Justin Aboud <justin.aboud@icloud.com>
2024-05-14 23:21:55 +1000

    Moved tabview back to app struct
    Moved tabview back to app struct

--- Product Saver/Product_SaverApp.swift
+++ Product Saver/Product_SaverApp.swift
@@ -11,22 +11,42 @@ import SwiftData
 @main
 struct Product_SaverApp: App {
 
+    @State private var selectedTab = 0
     @StateObject private var settings = Settings()
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .modelContainer(for: Product.self)
-                .environmentObject(settings)
-                .font(.system(size: settings.fontSize.value))
-                .tint(settings.tintColors)
-                .onAppear(){
-                    UILabel.appearance(whenContainedInInstancesOf: [UINavigationBar.self]).adjustsFontSizeToFitWidth = true
-                    settings.applyColorScheme()
+            TabView(selection: $selectedTab) {
+                ContentView()
+                .tabItem {
+                    Label("Home", systemImage: selectedTab == 0 ? "house.fill" : "house")
+                        .environment(\.symbolVariants, .none)
                 }
-                .onReceive(NotificationCenter.default.publisher(for: UIApplication.willEnterForegroundNotification)) { _ in
-                    settings.applyColorScheme()
-                }
+                .tag(0)
+                    CategoriesView()
+                        .tabItem {
+                            Label("Category", systemImage: selectedTab == 1 ? "folder.fill" : "folder")
+                                .environment(\.symbolVariants, .none)
+                        }
+                        .tag(1)
+                    SettingsView(settings: settings)
+                        .tabItem {
+                            Label("Settings", systemImage: selectedTab == 2 ? "gearshape.fill" : "gearshape")
+                                .environment(\.symbolVariants, .none)
+                        }
+                        .tag(2)
+            }
+            .modelContainer(for: Product.self)
+            .environmentObject(settings)
+            .font(.system(size: settings.fontSize.value))
+            .tint(settings.tintColors)
+            .onAppear(){
+                UILabel.appearance(whenContainedInInstancesOf: [UINavigationBar.self]).adjustsFontSizeToFitWidth = true
+                settings.applyColorScheme()
+            }
+            .onReceive(NotificationCenter.default.publisher(for: UIApplication.willEnterForegroundNotification)) { _ in
+                settings.applyColorScheme()
+            }
         }
     }
 }
